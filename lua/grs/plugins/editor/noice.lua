3034167cb56270b9b46b50dfb2dff1fd2a31c16c
grscheller <geoffrey@scheller.com>
2023-06-01 17:00:53 -0600

    Messing with noice settings.
    In editor/noice.lua, I
    - turned off classic bottom search:
        bottom_search = false
    - turned on boarders to hover docs & signature help:
        lsp_dpc_border = true
    
    Generally I like both changes.  The only thing I need to adjust with
    Noice is that when editing near the top of a file, nvim-notify windows
    will rollover what I am trying to edit.

--- lua/grs/plugins/editor/noice.lua
+++ lua/grs/plugins/editor/noice.lua
@@ -18,12 +18,13 @@ return {
                ['cmp.entry.get_documentation'] = true,
             },
          },
+         -- TODO: see smjonas/inc-rename.nvim 
          presets = {
-            bottom_search = true,  -- use "classic" botton cmdline for search
+            bottom_search = false,  -- use "classic" botton cmdline for search
             command_palette = true,  -- position the cmdline & poppupmenu together
             long_message_to_split = true,  -- long messages sent to a split
             inc_rename = false,  -- enable input dialog for inc-rename.nvim
-            lsp_doc_border = false,  -- add borders to hover docs & signature help
+            lsp_doc_border = true,  -- add borders to hover docs & signature help
          },
       },
    }
