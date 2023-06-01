7811772c6de1f86cc3e86c84e2a1a1350aff0d53
grscheller <geoffrey@scheller.com>
2023-06-01 13:17:38 -0600

    Updated options.lua & globals.lua
    - big change was adjustments made to scrolloff & sidescrolloff
    - improvements for :set list
    - turned Perl configuration off in globals.lua

--- lua/grs/config/globals.lua
+++ lua/grs/config/globals.lua
@@ -1,7 +1,6 @@
 --[[ Globals - loaded before lazy takes control ]]
 
--- Loaded very early in init.lua,
--- most plugins only read these on startup.
+-- Loaded very early in init.lua, most plugins only read these on startup.
 
 -- Set leader keys once and for all
 vim.g.mapleader = ' '
@@ -12,3 +11,6 @@ vim.g.python3_host_prog = string.format(
    '%s/.local/share/pyenv/shims/python',
    os.getenv 'HOME'
 )
+
+-- Perl had its day
+vim.g.loaded_perl_provider = 0
--- lua/grs/config/options.lua
+++ lua/grs/config/options.lua
@@ -1,7 +1,5 @@
 --[[ Options - loaded before lazy takes control ]]
 
--- loaded very early
-
 -- string, numeric  & boolean options
 local options = {
 
@@ -14,47 +12,47 @@ local options = {
    fileformats = 'unix,mac,dos',
 
    -- Set default tab/indenting behavior
-   tabstop = 4,       -- tab stops every 4 spaces, not size of tab character
-   shiftwidth = 4,    -- number of columns used for auto-indentation
-   softtabstop = 4,   -- <tab>/<bs> inserts/deletes 4 columns ws
+   tabstop = 4,      -- tab stops every 4 spaces, not size of tab character
+   shiftwidth = 4,   -- number of columns used for auto-indentation
+   softtabstop = 4,  -- <tab>/<bs> inserts/deletes 4 columns ws
    expandtab = true,  -- use only spaces when tabbing or auto-indenting
 
    -- Buffer/Editing preferences
    hidden = true,      -- my expectations are that buffers don't get abandoned
    joinspaces = true,  -- use 2 spaces when joining sentences
    nrformats = 'bin,hex,octal,alpha',  -- for <c-a> & <c-x>
-   undofile = true,             -- save undo history in ~/.local/share/nvim/undo/
-   ignorecase = true,           -- Case insensitive search when given
-   smartcase = true,            -- just lower case search patterns.
-   textwidth = 80,              -- keep comments & code horizontally under control
-   colorcolumn = '+1,+21,+41',  -- keep comments <= 80 code <= 100, data <= 120 
-   formatoptions = 'cqjp1',     -- for joining and breaking comment lines
-   spelloptions = 'camel',      -- spellCheckCamelCaseComponents
-   cpoptions = 'aABcfFJs',      -- the default is 'aABceFs_', cw & ce now differ
+   undofile = true,    -- save undo history in ~/.local/share/nvim/undo/
+   ignorecase = true,  -- Case insensitive search when given
+   smartcase = true,   -- just lower case search patterns.
+   textwidth = 80,     -- keep comments & code horizontally under control
+   colorcolumn = '+1,+21,+41',  -- keep comments <= 80 code <= 100, data <= 120
+   formatoptions = 'cqjp1',  -- for joining and breaking comment lines
+   spelloptions = 'camel',   -- spellCheckCamelCaseComponents
+   cpoptions = 'aABcfFJs',   -- the default is 'aABceFs_', cw & ce now differ
 
    -- Windowing preferences
-   mouse = 'a',              -- enable mouse for all modes
-   ruler = false,            -- disable ruler
-   splitbelow = true,        -- horizontally split window below
-   splitright = true,        -- vertically split window to right
-   number = false,           -- initially no window line numbering
+   mouse = 'a',        -- enable mouse for all modes
+   ruler = false,      -- disable ruler
+   splitbelow = true,  -- horizontally split window below
+   splitright = true,  -- vertically split window to right
+   number = false,     -- initially no window line numbering
    relativenumber = false,   -- initially no window relative line numbering
-   wrap = false,             -- don't wrap lines
-   scrolloff = 1,            -- try to keep cursor off of top/bottom line of window
-   sidescrolloff = 4,        -- try to keep cursor away from side of window
-   sidescroll = 1,           -- horizontally scroll one character at a time
+   wrap = false,       -- don't wrap lines
+   scrolloff = 10,     -- try to keep cursor off of top/bottom line of window
+   sidescrolloff = 8,  -- try to keep cursor away from side of window
+   sidescroll = 1,     -- horizontally scroll one character at a time
 
    -- Settings affecting LSP clients & plugins
-   timeoutlen = 1200,                      -- ms to wait for key mapped sequence to complete
-   updatetime = 400,                       -- ms of no cursor movement to trigger CursorHold event
-   signcolumn = 'yes',                     -- fixes first column, reduces jitter
-   showmode = false,                       -- redundant with Lualine
-   showcmd = false,                        -- redundant with WhichKey
+   termguicolors = true,  -- enable 24-bit RGB color for ISO-8613-3 terminals
+   timeoutlen = 1200,  -- ms to wait for key mapped sequence to complete
+   updatetime = 400,   -- ms of no cursor movement to trigger CursorHold event
+   signcolumn = 'yes', -- fixes first column, reduces jitter
+   showmode = false,   -- redundant with Lualine
+   showcmd = false,    -- redundant with WhichKey
+   wildmenu = false,   -- using hrsh7th/cmp-cmdline for this
+   shortmess = 'atToOc',  -- shorten statusline - removed F for Scala Metals
+   complete = '.,w,b,u,kspell',  -- no "t,i" redundant with LSP
    completeopt = 'menu,menuone,noselect',  -- for nvim-cmp
-   termguicolors = true,                   -- enable 24-bit RGB color for ISO-8613-3 terminals
-   complete = '.,w,b,u,kspell',            -- no "t,i" - redundant with LSP
-   shortmess = 'atToOc',                   -- shorten statusline - removed F for Scala Metals
-   wildmenu = false,                       -- using hrsh7th/cmp-cmdline for this
 
    -- Essentially disable folding
    foldenable = false,     -- Allows the repurposing of folding
@@ -63,13 +61,14 @@ local options = {
 }
 
 for k, v in pairs(options) do
-   vim.opt[k] = v
+vim.opt[k] = v
+	vim.opt[k] = v
 end
 
 -- list & dictionary options
-vim.opt.matchpairs:append { '<:>' }    -- additional symbols for '%' matching
-vim.opt.iskeyword:append { '-' }       -- adds snake-case to word motions
-vim.opt.listchars = {                  -- for :list, :set list, :set nolist
+vim.opt.matchpairs:append { '<:>' }  -- additional symbols for '%' matching
+vim.opt.iskeyword:append { '-' }     -- adds snake-case to word motions
+vim.opt.listchars = {                -- for :list, :set list, :set nolist
    space = '_',
    trail = '*',
    nbsp = '+',
