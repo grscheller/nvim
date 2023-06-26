0ee3711e00d4448c093880a0cd04489b5464c609
grscheller <geoffrey@scheller.com>
2023-06-25 21:18:52 -0400

    Updated plugins/editor/whichkey.lua
    - removed prefix for now uninstalled Mason

--- lua/grs/plugins/editor/whichkey.lua
+++ lua/grs/plugins/editor/whichkey.lua
@@ -17,14 +17,13 @@ return {
             }
          }
 
-   -- for Which-Key menus
+         -- for Which-Key menus
          wk.register({ name = '<space>' }, { prefix = '<leader>' })
          wk.register({ name = '\\' }, { prefix = '\\', mode = { 'n', 'x' } })
          wk.register({ name = 'dap' }, { prefix = '\\\\' })
          wk.register({ name = 'lazy' }, { prefix = '<leader>l' })
          wk.register({ name = 'lsp+g' }, { prefix = 'g' })
          wk.register({ name = 'lsp+z' }, { prefix = 'z' })
-         wk.register({ name = 'mason' }, { prefix = '<leader>m' })
          wk.register({ name = 'harpoon' }, { prefix = '<leader>h' })
          wk.register({ name = 'telescope' }, { prefix = '<leader>t', mode = 'n' })
          wk.register({ name = 'system clipboard' }, { prefix = '\\s', mode = { 'n', 'x' } })
